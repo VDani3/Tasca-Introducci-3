80e8c6ee4cd02d91e64441fd971b9f4803effef9
jmartint13 <joelmt04yt@gmail.com>
2022-11-30 17:40:54 +0100

    inputs de los pares

---
+++ prueba.py
@@ -0,0 +1,9 @@
+
+apellido1 = input("Inserta el primer apellido: ")
+
+tlfn = input("Inserta el numero de telefono: ")
+
+curso = input("Inserta el curso: ")
+
+nacimiento = input("Inserta la fecha de nacimiento ((DD-MM-YYYY))")
+
